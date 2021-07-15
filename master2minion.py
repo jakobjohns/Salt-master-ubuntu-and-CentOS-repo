d24abfd88e5d021d1ca8136c2d1a3d6a73478317
Jakob <jakobjohns5@gmail.com>
2021-07-15 16:06:04 -0400

    commit on 'main'

--- master2minion.py
+++ master2minion.py
@@ -45,9 +45,8 @@ def mount():
                 input("Press enter to continue...")
                 break
             elif (a == 2):
-                fs_type = input("Please enter the type of filesystem: (ext4, xfs")
+                fs_type = input("Please enter the type of filesystem: (ext4, xfs, etc.): ")
                 mount_point = input("What is the path of the mount point? (Your repo will be created here) ")
-                fullmountpoint = "sudo mount -t " + fs_type + " " + parti + " " + mount_point
                 break
             elif (a == 3):
                 mayexit = False
